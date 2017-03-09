5e02604d26f956bcd0eb6e3493def50fe9e176a5
JAMG540 <jamg54@live.com.mx>
2017-03-09 00:45:28 -0600

    Update MainActivity.java

--- app/src/main/java/edu/cecyt9/ipn/practica1_calculadora/MainActivity.java
+++ app/src/main/java/edu/cecyt9/ipn/practica1_calculadora/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.math.*;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
@@ -27,7 +28,9 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    Double numero1,numero2,resultado;
+    Double numero1=0.0;
+    Double numero2=0.0;
+    Double resultado=0.0;
     String operador;
 
     public void onClickButtonUno(View miView)
@@ -84,14 +87,29 @@ public class MainActivity extends ActionBarActivity {
     public void onClickButtonPunto(View miView)
     {
         TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
-        tv.setText(tv.getText() + ".");
+        if(tv.equals(".")){
+            android.widget.Toast.makeText(this,"Ya se incertó un punto anteriormente", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            tv.setText(tv.getText() + ".");
+        }
+
     }
 
     public void onClickOperacionCapturaNumero1(View miView)
     {
-        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
-        numero1 = Double.parseDouble(tv.getText().toString());
-        tv.setText("");
+        try {
+            TextView tv = (TextView) findViewById(R.id.textViewNumero);
+            numero1 = Double.parseDouble(tv.getText().toString());
+            tv.setText("");
+        }catch (Exception e){
+
+            android.widget.Toast.makeText(this,"Introduce un número", Toast.LENGTH_SHORT).show();
+
+        }
+
+
     }
 
     public void onClickSuma(View miView)
@@ -115,25 +133,90 @@ public class MainActivity extends ActionBarActivity {
         onClickOperacionCapturaNumero1(miView);
     }
 
+    public void onClickRaiz(View miView)
+    {
+        operador = "Raiz";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickPotencia(View miView){
+        operador = "Potencia";
+        onClickOperacionCapturaNumero1(miView);
+
+    }
+    public void onClickSeno(View miView)
+    {
+        operador = "Sen";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickCoseno(View miView)
+    {
+        operador = "Cos";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickTangente(View miView)
+    {
+        operador = "Tan";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+
+
+
     public void onClickIgual(View miView)
     {
-        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
-        numero2 = Double.parseDouble(tv.getText().toString());
-
+        TextView tv;
+        String mensaje="";
         try {
 
-            if (operador.equals("+")) {
-                resultado = numero1 + numero2;
-            } else if (operador.equals("-")) {
-                resultado = numero1 - numero2;
-            } else if (operador.equals("*")) {
-                resultado = numero1 * numero2;
-            } else if (operador.equals("/")) {
-                resultado = numero1 / numero2;
+            tv = (TextView) findViewById(R.id.textViewNumero);
+
+            if(!operador.equals("Sen") & !operador.equals("Cos") & !operador.equals("Tan") & !operador.equals("Raiz") ) {
+
+
+             try {
+                 numero2 = Double.parseDouble(tv.getText().toString());
+
+                  if (operador.equals("+")) {
+                      resultado = numero1 + numero2;
+                  } else if (operador.equals("-")) {
+                      resultado = numero1 - numero2;
+                  } else if (operador.equals("*")) {
+                      resultado = numero1 * numero2;
+                  } else if (operador.equals("/")) {
+                      resultado = numero1 / numero2;
+                  } else if (operador.equals("Potencia")) {
+                      resultado = Math.pow(numero1, numero2);
+                  }
+              }catch (Exception e)
+              {
+                  mensaje="Asegúrate de introducir correctamente el operador y los números";
+              }
             }
+         else {
+                try {
+
+                    if (operador.equals("Raiz")) {
+                        resultado = Math.sqrt(numero1);
+                    } else if (operador.equals("Sen")) {
+                        resultado = Math.sin(numero1);
+                    } else if (operador.equals("Cos")) {
+                        resultado = Math.cos(numero1);
+                    } else if (operador.equals("Tan")) {
+                        resultado = Math.tan(numero1);
+                    }
+                }catch (Exception e){
+                    mensaje="Presiona un número primero.";
+                }
+            }
+
+
             tv.setText(resultado.toString());
         }catch(NumberFormatException nfe){
-            Toast.makeText(this,"Numero Incorrecto", LENGTH_SHORT).show();
+
+            Toast.makeText(this,mensaje, LENGTH_SHORT).show();
         }
     }
 
